5537eeb0ac2eb3bd96845c0f3d5c62082b1387d1
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 17:23:06 -0400

    reduce tol

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,12 @@ using NumericalMethods
 using Test
 
 @testset "Differentiation" begin
+    const at = 1e-4
     x = collect(range(0, 2π, step=π/4))
     for x_i in x
-        @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i) atol = 1e-4
-        @test Deriv.differentiate(sin, x_i) ≈ cos(x_i) atol = 1e-4
-        @test Deriv.twice_differentiate(sin, x_i) ≈ -sin(x_i) atol = 1e-4
+        @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i) atol = at
+        @test Deriv.differentiate(sin, x_i) ≈ cos(x_i) atol = at
+        @test Deriv.twice_differentiate(sin, x_i) ≈ -sin(x_i) atol = at
     end
     f(x) = x[1]^2 + x[2]^2
     f_1(x) = 2x[1]
@@ -17,8 +18,8 @@ using Test
     for x_i in x
         for y_i in x
             z_i = [x_i, y_i]
-            @test Deriv.gradient(f, z_i) ≈ J(z_i) atol = 1e-4
-            @test Deriv.hessian(f, z_i) ≈ H(z_i) atol = 1e-4
+            @test Deriv.gradient(f, z_i) ≈ J(z_i) atol = at
+            @test Deriv.hessian(f, z_i) ≈ H(z_i) atol = at
         end
     end
 end
