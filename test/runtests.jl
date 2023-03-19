23f99e7568f3c29c636faee857c527bd2ee07f97
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 17:15:03 -0400

    expand tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "Differentiation" begin
     x = collect(range(0, 2π, step=π/4))
-    for x_i in x[1]
+    for x_i in x
         @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i)
         @test Deriv.differentiate(sin, x_i) ≈ cos(x_i)
         @test Deriv.twice_differentiate(sin, x_i) ≈ -sin(x_i)
@@ -14,8 +14,8 @@ using Test
     f_zz(x) = 2
     J(x) = [f_1(x), f_2(x)]
     H(x) = [f_zz(x) 0; 0 f_zz(x)]
-    for x_i in x[1]
-        for y_i in x[1]
+    for x_i in x
+        for y_i in x
             z_i = [x_i, y_i]
             @test Deriv.gradient(f, z_i) ≈ J(z_i)
             @test Deriv.hessian(f, z_i) ≈ H(z_i)
