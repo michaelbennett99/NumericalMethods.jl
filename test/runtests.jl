5b344203303502eefd72eeee6980fa16af2b987e
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 17:25:30 -0400

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,12 @@ using Test
 
 @testset "Differentiation" begin
     at = 1e-4
+    rt = 1e-4
     x = collect(range(0, 2π, step=π/4))
     for x_i in x
-        @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i) atol = at
-        @test Deriv.differentiate(sin, x_i) ≈ cos(x_i) atol = at
-        @test Deriv.twice_differentiate(sin, x_i) ≈ -sin(x_i) atol = at
+        @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i) atol = at rtol = rt
+        @test Deriv.differentiate(sin, x_i) ≈ cos(x_i) atol = at rtol = rt
+        @test Deriv.twice_differentiate(sin, x_i) ≈ -sin(x_i) atol = at rtol = rt
     end
     f(x) = x[1]^2 + x[2]^2
     f_1(x) = 2x[1]
@@ -18,8 +19,8 @@ using Test
     for x_i in x
         for y_i in x
             z_i = [x_i, y_i]
-            @test Deriv.gradient(f, z_i) ≈ J(z_i) atol = at
-            @test Deriv.hessian(f, z_i) ≈ H(z_i) atol = at
+            @test Deriv.gradient(f, z_i) ≈ J(z_i) atol = at rtol = rt
+            @test Deriv.hessian(f, z_i) ≈ H(z_i) atol = at rtol = rt
         end
     end
 end
