d4977113c89506d70863b0e2d7de8a37900c7173
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 16:46:52 -0400

    add export statements

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -9,4 +9,8 @@ include("Min.jl")
 
 using .Roots, .Min
 
+export numderiv_one_side, numderiv_two_side, numderiv_second, gradient, hessian
+export Roots.newton, Roots.secant, Roots.bisect, Roots.brent
+export Min.newton, Min.brent
+
 end # module
