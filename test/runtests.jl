3587def4cb9274951247bfbc2a0e8ecd482b6acc
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 17:23:39 -0400

    remove const

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using NumericalMethods
 using Test
 
 @testset "Differentiation" begin
-    const at = 1e-4
+    at = 1e-4
     x = collect(range(0, 2π, step=π/4))
     for x_i in x
         @test Deriv.differentiate(sin, x_i; two_side=false) ≈ cos(x_i) atol = at
