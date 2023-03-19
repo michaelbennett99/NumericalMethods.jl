23bdde51c6e7d821a9bc58d99570f5010160c198
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 16:43:12 -0400

    remove all exports

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -3,9 +3,6 @@ module NumericalMethods
 using ForwardDiff, LinearAlgebra
 using .Roots, .Min
 
-export numderiv_one_side, numderiv_two_side, numderiv_second, gradient, hessian
-export bisect, secant, brent, newton
-
 include("Utils.jl")
 include("Deriv.jl")
 include("Roots.jl")
