e7940cb4ca5af5b3f84667908e8f94910cc711e3
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-03-19 12:19:49 -0400

    add module based exports

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -2,12 +2,13 @@ module NumericalMethods
 
 using ForwardDiff, LinearAlgebra
 
-export numderiv_one_side, numderiv_two_side, numderiv_second
-export gradient, hessian
-export bisect, secant, func_iter, newton, brent
+export numderiv_one_side, numderiv_two_side, numderiv_second, gradient, hessian
+export Roots.bisect, Roots.secant, Roots.brent, Roots.newton
+export Min.brent, Min.newton
 
 include("Utils.jl")
 include("Deriv.jl")
 include("Roots.jl")
+include("Min.jl")
 
 end # module
